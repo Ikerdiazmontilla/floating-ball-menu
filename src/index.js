33fa9243cd560c1772099fcccbf6a22c0161a654
Ikerdiazmontilla <ikercorrevuela@gmail.com>
2024-02-03 17:23:30 -0600

    Create and style ball, add eventlisteners

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import "./style.css"
 
+
+
+const closeMenu = function eventListener(event) {
+    event.currentTarget.classList.remove("clicked")
+    event.currentTarget.removeEventListener("click",closeMenu)
+    event.currentTarget.addEventListener("click", openMenu)
+}
+
+const openMenu = function eventListener(event) {
+    event.currentTarget.classList.add("clicked")
+    event.currentTarget.removeEventListener("click", openMenu)
+    event.currentTarget.addEventListener("click", closeMenu)
+}
+
+const ball = document.querySelector(".ball")
+ball.addEventListener("click", openMenu)
