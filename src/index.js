35ddcf7aa8ef673c46336b40d69e706cee8c9e47
Ikerdiazmontilla <ikercorrevuela@gmail.com>
2024-02-04 19:05:31 -0600

    Add transitions and javascript

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import "./style.css"
 
-
-
 const closeMenu = function eventListener(event) {
     event.currentTarget.classList.remove("clicked")
     event.currentTarget.removeEventListener("click",closeMenu)
     event.currentTarget.addEventListener("click", openMenu)
+    const items = document.querySelectorAll(".item")
+    items.forEach((item) => {
+        item.classList.remove("show")
+    })
 }
 
 const openMenu = function eventListener(event) {
     event.currentTarget.classList.add("clicked")
     event.currentTarget.removeEventListener("click", openMenu)
     event.currentTarget.addEventListener("click", closeMenu)
+    const items = document.querySelectorAll(".item")
+    items.forEach((item) => {
+        item.classList.add("show")
+    })
 }
 
 const ball = document.querySelector(".ball")
